aa7fab9473350a0a6e5295b366117816dfdb955f
Eric <nextstep@ericdee.me>
2021-12-24 14:40:45 -0700

    Completed for peer review

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  // This section gets all of the elements from the html
+  var principal = document.getElementById("principal").value;
+  // This logic validates that the principal amount is a rational number, and resets the window focus if it isn't
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    return false;
+  }
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = (principal * years * rate) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+  var result = document.getElementById("result");
+
+  // This section applies the retrieved and calculated values to the output location
+  result.innerHTML = `<br>\
+    If you deposit <span style="background-color: #ffff00">${principal}</span>,<br>\
+    at an interest rate of <span style="background-color: #ffff00">${rate}</span>.<br>\
+    You will receive an amount of <span style="background-color: #ffff00">${interest}</span>,<br>\
+    in the year <span style="background-color: #ffff00">${year}</span><br>`;
+
+  // Returns true on success
+  return true;
 }
-        
+
+function updateRate() {
+  // This function gets the rate value and updates the span directly after it every time it gets changed
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval + "%";
+}
